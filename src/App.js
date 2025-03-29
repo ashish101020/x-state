c4cef4d48786c31f95cbf53cb2bb46eda0608fa2
ashish <guptashish101020@gmail.com>
2025-03-29 11:34:30 +0530

    update dropDown syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import LocationSelector from './components/LocationSelector';
+// import LocationSelector from './components/LocationSelector';
+import DropDownComponent from './components/DropDownComponent';
 
 function App() {
   return (
     <div className="App">
-      <h1>Select Location</h1>
-      <LocationSelector/>
+      {/* <h1>Select Location</h1> */}
+      {/* <LocationSelector/> */}
+      <DropDownComponent/>
     </div>
   );
 }
